bc613f203fc34763d9c31a8077af2e1e62db0ca6
Frankenstei <enwereloveday@yahoo.com>
2023-03-12 09:11:27 -0700

    a

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This defines all common
 attributes/methods for other classes
 """
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
@@ -20,14 +20,14 @@ class BaseModel():
                 elif key == 'created_at':
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if 'id' not in kwargs.keys():
-                    self.id = str(uuid.uuid4())
+                    self.id = str(uuid4())
                 if 'created_at' not in kwargs.keys():
                     self.created_at = datetime.now()
                 if 'updated_at' not in kwargs.keys():
                     self_updated_at = datetime.now()
                     setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
@@ -35,14 +35,14 @@ class BaseModel():
     def __str__(self):
         """Simply prints class name, id, dict
         """
-        print(
-                "[", __class__.__name__, "] (",
-                str(self.id), ") ", str(self.__dict__))
+        return(
+                "[" +  __class__.__name__ + "] (" +
+                str(self.id), ") " + str(self.__dict__))
 
     def save(self):
         """save"""
         self.updated_at = datetime.now()
-        models.storage.save(self)
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary"""
@@ -51,7 +51,3 @@ class BaseModel():
         base_dict['created_at'] = self.created_at.isoformat()
         base_dict['updated_at'] = self.updated_at.isoformat()
         print(base_dict)
-
-
-if __name__ == '__main__':
-    BaseModel().to_dict()
